649681113f0a57d0f1963c41f4abc5d25db3c18f
zybron <zybron@gmail.com>
2019-05-14 19:03:02 -0400

    Move copy to clipboard function to footer

--- footer.php
+++ footer.php
@@ -63,7 +63,26 @@
 		}
 		return 0;
 	}
-	
+
+	function copyToClipboard(e) {
+	    var tempItem = document.createElement('input');
+
+	    tempItem.setAttribute('type','text');
+	    tempItem.setAttribute('display','none');
+
+	    let content = e;
+	    if (e instanceof HTMLElement) {
+			content = e.innerHTML;
+	    }
+
+	    tempItem.setAttribute('value',content);
+	    document.body.appendChild(tempItem);
+
+	    tempItem.select();
+	    document.execCommand('Copy');
+
+	    tempItem.parentElement.removeChild(tempItem);
+	}
 	
 </script>
 
